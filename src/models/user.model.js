2b980de2ce9f3173bec42a6f166abdfa77854ec5
Khalid Hussain <khalid.ghulbi22@gmail.com>
2024-02-16 18:01:15 +0500

    jwt and hash

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -63,5 +63,32 @@ userSchema.pre("save", async function (next) {
     next()
 })
 
+// generate token for user 
+userSchema.methods.generateAccessToken = function(){ 
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
 
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 export const User = mongoose.model("User", userSchema)
