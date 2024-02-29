7474ea8da5a4f342beede973df932b19763a4101
Khalid Hussain <khalid.ghulbi22@gmail.com>
2024-02-29 12:52:32 +0500

    check for username with same email already existed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,33 +1,47 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {ApiError} from "../utils/ApiError.js"
-
-const registerUser = asyncHandler( async (req, res)=>{ 
-    //get user deatail form frontend
-    //validte data
-    //check if user already exist: usernmae & email
-    //chck for image/check for avator
-    //upload them to cloudinary and get the url, avator
-    //create user object -createentry in db
-    //remove password and refresh token token from response
-    //check for user cration
-    //return response
+import { ApiError } from "../utils/ApiError.js";
+import { User} from "../models/user.model.js"
 
 
-    const { username, email, fullName, password } = req.body; // we get these from fronrend and it coems in json format in body, so we use req.body to get the data
-    console.log("email: ", email)
+const registerUser = asyncHandler(async (req, res) => {
+  //get user deatail form frontend
+  //validte data
+  //check if user already exist: usernmae & email
+  //chck for image/check for avator
+  //upload them to cloudinary and get the url, avator
+  //create user object -createentry in db
+  //remove password and refresh token token from response
+  //check for user cration
+  //return response
 
-    //validate data we chck for validations
+  const { username, email, fullName, password } = req.body; // we get these from fronrend and it coems in json format in body, so we use req.body to get the data
+  console.log("email: ", email);
 
-    // if (fullName === "" ) {
-    //     throw ApiError(400, "fullName is required")
-    // } 
-    
-    //now check for validation and use some method to check if any of the field is empty and then trim the field and check and throw error 
-    if (
-        [fullName, email, username, password].some((fld) => fld?.trim() === "")
-    ) {
-        throw new ApiError(400, "All fields are required")
-    }
+  //validate data we chck for validations
+
+  // if (fullName === "" ) {
+  //     throw ApiError(400, "fullName is required")
+  // }
+
+  //now check for validation and use some method to check if any of the field is empty and then trim the field and check and throw error
+  if ([fullName, email, username, password].some((fld) => fld?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const emailRegex = /^\S+@\S+\.\S+$/;
+  if (!emailRegex.test(email)) {
+    throw new ApiError(400, "Invalid email format");
+  }
+
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }]
+})
+
+if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists")
+}
 });
 
-export { registerUser }
+
+
+export { registerUser };
