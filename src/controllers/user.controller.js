ad4f7181d44aad638babfab9fda97fff5e0de5b6
Khalid Hussain <khalid.ghulbi22@gmail.com>
2024-02-29 12:33:26 +0500

    check for validationand use some mthod and throw ApiError

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
 
 const registerUser = asyncHandler( async (req, res)=>{ 
     //get user deatail form frontend
@@ -14,6 +15,19 @@ const registerUser = asyncHandler( async (req, res)=>{
 
     const { username, email, fullName, password } = req.body; // we get these from fronrend and it coems in json format in body, so we use req.body to get the data
     console.log("email: ", email)
+
+    //validate data we chck for validations
+
+    // if (fullName === "" ) {
+    //     throw ApiError(400, "fullName is required")
+    // } 
+    
+    //now check for validation and use some method to check if any of the field is empty and then trim the field and check and throw error 
+    if (
+        [fullName, email, username, password].some((fld) => fld?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
 });
 
 export { registerUser }
